870a3474a3180cfc318b604ccb037c0d98d05446
Niki <nikniksham@gmail.com>
2020-10-05 15:58:53 +0300

    1 задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[][] x = new int[scan.nextInt()][scan.nextInt()];
+        int x = scan.nextInt(), y = scan.nextInt();
+        int[][] m = new int[x][y];
+        fillMatrix(m, scan);
+        System.out.println(y + " " + x);
+        printMatrix1(m);
+    }
 
+    public static void fillMatrix(int[][] x, Scanner scan) {
+        for (int i = 0; i < x.length; i++) {for (int j = 0; j < x[i].length; j++) {x[i][j] = scan.nextInt();}}}
 
-    }}
+    public static void printMatrix(int[][] x) {
+        for (int i = 0; i < x.length; i++) {for (int j = 0; j < x[i].length; j++) {System.out.print(x[i][j] + " ");}System.out.println();}}
+
+    public static void printMatrix1(int[][] x) {
+        for (int j = 0; j < x[0].length; j++) {for (int i = x.length - 1; i >= 0; i--) {System.out.print(x[i][j] + " ");}System.out.println();}}
+
+    }
