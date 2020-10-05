8994c5ad0b8a6b7bd31666563f682a53d6100293
Niki <nikniksham@gmail.com>
2020-10-05 16:04:03 +0300

    2 задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt(), y = scan.nextInt();
-        int[][] m = new int[x][y];
-        fillMatrix(m, scan);
-        System.out.println(y + " " + x);
-        printMatrix1(m);
+        int x = scan.nextInt();
+        int[][] m = new int[x][x];
+        fillMatrix2(m);
+        printMatrix(m);
     }
 
     public static void fillMatrix(int[][] x, Scanner scan) {
         for (int i = 0; i < x.length; i++) {for (int j = 0; j < x[i].length; j++) {x[i][j] = scan.nextInt();}}}
+
+    public static void fillMatrix2(int[][] x) {
+        int l = x.length;
+        for (int i = 0; i < x.length; i++) {for (int j = 0; j < x[i].length; j++) {
+            if (i + j == l - 1) {x[i][j] = 1;}
+            else if (i + j > l - 1) {x[i][j] = 2;}}}}
 
     public static void printMatrix(int[][] x) {
         for (int i = 0; i < x.length; i++) {for (int j = 0; j < x[i].length; j++) {System.out.print(x[i][j] + " ");}System.out.println();}}
