4a08f0a4ed420896be4280d114ec4374951982eb
Niki <nikniksham@gmail.com>
2020-10-19 16:07:49 +0300

    ООП 2.2.1

--- src/com/company/_Double.java
+++ src/com/company/_Double.java
@@ -1,11 +1,5 @@
 package com.company;
 
-import java.util.Scanner;
-
-/*
-
-package com.company;
-
 public class _Double {
     private double self;
 
@@ -18,15 +12,4 @@ public class _Double {
     public double getSelf() {return self; }
 
     public void setSelf(double self) { this.self = self; }
-}
-
- */
-public class Main {
-    public static void main(String[] args) {
-        _Double a = new _Double(12.4);
-        System.out.println(a.multiplication(3.2));
-        System.out.println(a.division(3.2));
-        System.out.println(a.add(3.2));
-        System.out.println(a.Subtraction(3.2));
-    }
 }
